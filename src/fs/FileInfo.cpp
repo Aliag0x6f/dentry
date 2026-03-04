d8672c1736d8b25b40735352c0df394142fcbb52
Hugo Fabresse <hugo.fabresse@epitech.eu>
2026-03-04 13:46:05 +0100

    feat(fs): implement FileInfo

---
+++ src/fs/FileInfo.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file FileInfo.cpp
+ * @brief Implementation of FileInfo.
+ *
+ * @author Hugo Fabresse
+ */
+
+#include "FileInfo.h"
+#include "MimeResolver.h"
+#include "../util/DateFormatter.h"
+#include "../util/PermissionFormatter.h"
+#include "../util/SizeFormatter.h"
+
+namespace Dentry::Fs {
+
+	FileInfo::FileInfo(const QString &path) :
+		FileInfo(QFileInfo(path)) {}
+
+	FileInfo::FileInfo(const QFileInfo &fileInfo) :
+		m_fileInfo(fileInfo) { compute(); }
+
+	void FileInfo::compute() {
+		m_name				   = m_fileInfo.fileName();
+		m_absolutePath		   = m_fileInfo.absoluteFilePath();
+		m_size				   = m_fileInfo.isDir() ? 0 : m_fileInfo.size();
+		m_lastModified		   = m_fileInfo.lastModified();
+		m_mimeType			   = MimeResolver::resolve(m_fileInfo);
+		m_formattedSize		   = m_fileInfo.isDir() ? QString() : Util::SizeFormatter::format(m_size);
+		m_formattedDate		   = Util::DateFormatter::format(m_lastModified);
+		m_formattedPermissions = Util::PermissionFormatter::format(m_fileInfo);
+	}
+
+} // namespace Dentry::Fs
