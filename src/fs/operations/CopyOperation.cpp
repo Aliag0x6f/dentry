39ed59765ba17c2df58bd566e31e6361291c214a
Hugo-Fabresse <hugo.fabresse@epitech.eu>
2026-03-11 14:21:37 +0100

    feat(fs): implement CopyOperation

---
+++ src/fs/operations/CopyOperation.cpp
@@ -0,0 +1,103 @@
+/**
+* @file CopyOperation.cpp
+ * @brief Implementation of CopyOperation.
+ *
+ * @author Hugo Fabresse
+ */
+
+#include "CopyOperation.h"
+
+#include <QDir>
+#include <QFile>
+#include <QFileInfo>
+#include <QtConcurrent>
+
+namespace Dentry::Fs {
+
+    CopyOperation::CopyOperation(const QStringList &sources,
+                                 const QString     &destination,
+                                 QObject           *parent)
+        : AFileOperation(parent)
+        , m_sources(sources)
+        , m_destination(destination) {}
+
+    void CopyOperation::execute() {
+        setRunning(true);
+
+        m_future = QtConcurrent::run([this] {
+            const int total = m_sources.count();
+            int completed = 0;
+
+            for (const QString &source : m_sources) {
+                if (isCancelled()) {
+                    emit finished(false, "Operation cancelled");
+                    setRunning(false);
+                    return;
+                }
+
+                const QFileInfo info(source);
+                const QString dest = m_destination + "/" + info.fileName();
+
+                bool success = false;
+
+                if (info.isDir()) {
+                    success = copyDir(source, dest);
+                } else {
+                    if (QFile::exists(dest))
+                        QFile::remove(dest);
+                    success = QFile::copy(source, dest);
+                }
+
+                if (!success) {
+                    emit finished(false, "Could not copy file " + dest);
+                    setRunning(false);
+                    return;
+                }
+
+                ++completed;
+                emit progress(static_cast<int>(completed * 100.0 / total));
+            }
+
+            emit finished(true, QString());
+            setRunning(false);
+        });
+    }
+
+    bool CopyOperation::copyDir(const QString &source, const QString &destination) {
+        QDir srcDir(source);
+        QDir dstDir;
+
+        if (!dstDir.mkpath(destination))
+            return false;
+
+        const QFileInfoList entries = srcDir.entryInfoList(
+            QDir::AllEntries | QDir::NoDotAndDotDot | QDir::Hidden
+        );
+
+        for (const QFileInfo &entry : entries) {
+            if (isCancelled())
+                return false;
+
+            const QString destPath = destination + "/" + entry.fileName();
+
+            if (entry.isDir()) {
+                if (!copyDir(entry.absoluteFilePath(), destPath))
+                    return false;
+            } else {
+                if (QFile::exists(destPath))
+                    QFile::remove(destPath);
+                if (!QFile::copy(entry.absoluteFilePath(), destPath))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    QString CopyOperation::description() const {
+        return QString("Copying %1 item(s) to %2")
+            .arg(m_sources.count())
+            .arg(m_destination);
+    }
+
+} // namespace Dentry::Fs
